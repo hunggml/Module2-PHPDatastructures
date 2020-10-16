be35566186b80780432ecd2117683b11495238ec
dvhungg123@gmail.com <dvhungg123@gmail.com>
2020-10-16 13:55:52 +0700

    update

---
+++ baitap/Square-Dance/Dancer.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class Dancer
+{
+    public $name;
+    public $gender;
+
+}
---
+++ demo.php
@@ -0,0 +1,5 @@
+<?php
+// Write your code below:
+echo "1. Teach PHP";
+echo "<br> Eat breakfast";
+echo "<br> Learn to have fun";
